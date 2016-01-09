eab7669ddf03f7ffc216f40e9321937d64717210
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-01-09 17:42:36 -0500

    Clear and center last site if there aren't two

--- Sites/views.py
+++ Sites/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 from Sites import models
 
 def all(request):
+    sites = models.Site.objects.all()
+    last_site_is_loner = (models.Site.objects.count() % 2 != 0)
     view_data = {
-        'sites': models.Site.objects.all()
+        'last_site_is_loner': last_site_is_loner,
+        'sites': sites
     }
     return render(request, 'all.html', view_data)
