7f83956343fc86fc577dccb6362a8b30c2c03930
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-01-19 19:24:39 -0500

    Use pseudo-elements instead of DOM elements for overlays

--- Sites/views.py
+++ Sites/views.py
@@ -3,7 +3,6 @@ from .forms import SayHelloForm
 from django.conf import settings
 from django.contrib import messages
 from django.core.mail import send_mail
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 def all(request):
