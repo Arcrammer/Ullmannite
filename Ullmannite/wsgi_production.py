44132e9db776ea14386ddc9920a6f03e7faefdde
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-05-14 09:56:53 -0400

    Update WSGI environment configs

--- Ullmannite/wsgi_production.py
+++ Ullmannite/wsgi_production.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append("/var/www/Ullmannite")
+sys.path.append("/var/www/Ullmannite/Ullmannite")
 
 from django.core.wsgi import get_wsgi_application
 
